9eff301f80d97a11ab73c30486cb61533082ba91
Temur Lomidze <temur.lomidze227@cve.tsu.edu.ge>
2025-11-30 17:06:53 +0400

    Fix: Added final CORS configuration for frontend testing

--- online-magazine-api/src/app.js
+++ online-magazine-api/src/app.js
@@ -12,7 +12,8 @@ const articleRoutes = require("./routes/articles");
 const app = express();
 connectDB();
 
-app.use(cors());
+app.use(cors({ origin: '*' })); 
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
